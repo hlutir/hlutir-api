9e799cb8228897e30e80325fd608d998edcb8643
Jonas Claes <jonasclaes2001@outlook.com>
2021-06-17 14:16:35 +0200

    Fixed jest warning; Added basic tests for devices;

--- src/devices/devices.service.spec.ts
+++ src/devices/devices.service.spec.ts
@@ -3,17 +3,42 @@ import { PrismaService } from '../prisma.service';
 import { DevicesService } from './devices.service';
 
 describe('DevicesService', () => {
+  let module: TestingModule;
   let service: DevicesService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
       providers: [DevicesService, PrismaService],
     }).compile();
 
     service = module.get<DevicesService>(DevicesService);
   });
 
+  afterEach(async () => {
+    await module.close();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find a device', async () => {
+    expect(await service.findOne({ deviceID: 1 })).toBeInstanceOf(Object);
+  });
+
+  it('should not find a device', async () => {
+    expect(await service.findOne({ deviceID: 0 })).toBeNull();
+  });
+
+  it('should find multiple devices', async () => {
+    expect((await service.findAll({})).length).toBeGreaterThan(1);
+  });
+
+  it('should find no devices', async () => {
+    expect((await service.findAll({ where: { deviceID: 0 } })).length).toBe(0);
+  });
+
+  it('should find one device', async () => {
+    expect((await service.findAll({ where: { deviceID: 1 } })).length).toBe(1);
   });
 });
