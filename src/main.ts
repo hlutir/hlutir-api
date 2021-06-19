42bea1d0a698692a2a6f8d4827abb5d647bff54c
Jonas Claes <jonasclaes2001@outlook.com>
2021-06-19 17:53:31 +0200

    Added CORS

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, documentBuilder);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
